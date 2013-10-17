9bc9da15e400a1e63836dabe353420ab35196bcb
Jeff Bickart <jeff@neposystems.com>
2013-10-17 16:13:21 -0400

    test commit from phpstorm

--- README.md
+++ README.md
@@ -2,4 +2,3 @@ SugarCRM6_To_SugarCRM7
 ======================
 
 An example SugarCRM 6 module that is converted to SugarCRM 7
-ABCDEF
---
+++ manifest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright 2013 NEPO Systems, LLC
+ *
+ * Twitter: @bickart
+ * Date: October 17, 2013
+ *
+ */
+
+$manifest = array(
+    'acceptable_sugar_versions' =>
+    array(
+        'exact_matches' =>
+        array(),
+        'regex_matches' =>
+        array(
+            0 => '6.3.[01]?',
+            1 => '6.4.[01]?',
+            2 => '6.5.[01]?',
+            3 => '6.6.[01]?',
+            4 => '6.7.[01]?',
+            5 => '6.8.[01]?',
+            6 => '6.9.[01]?',
+            7 => '7.0.[01]?',
+        ),
+    ),
+    'acceptable_sugar_flavors' =>
+    array(
+        'CE', 'PRO', 'ENT', 'CORP', 'ULT'
+    ),
+    'readme' => '',
+    'key' => '',
+    'author' => 'jeff @ neposystems dot com',
+    'description' => 'Example SugarCRM 6 Module converted to SugarCRM 7',
+    'icon' => '',
+    'name' => 'Simple Example',
+    'published_date' => 'Oct 18, 2013 5:00:00 PM EST',
+    'type' => 'module',
+    'version' => '0.0.1',
+    'remove_tables' => 'true',
+    'is_uninstallable' => 'true',
+);
